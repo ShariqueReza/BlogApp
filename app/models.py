202ec97180262b0efd3a265a3fe7e0e9c1b788b0
Sharique <shariquereza786@gmail.com>
2024-11-15 16:34:14 +0530

    made ManyToMany relation b/w Tag & Post model and did the migartion

--- app/models.py
+++ app/models.py
@@ -21,3 +21,4 @@ class Post(models.Model):
     last_updates=models.DateTimeField(auto_now=True)
     slug=models.SlugField(max_length=200,unique=True)
     image=models.ImageField(null=True,blank=True,upload_to="images/")
+    tags=models.ManyToManyField(Tag,blank=True,related_name='post')
