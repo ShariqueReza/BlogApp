e6bf5413761463e5391a43ea902f10b7308ba866
Sharique <shariquereza786@gmail.com>
2024-11-15 16:25:42 +0530

    created a model named Tag & will autogenerate the slug url also

--- app/models.py
+++ app/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Tag(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=200,unique=True)
+
+    def save(self,*args, **kwargs):
+        if not self.id:
+            self.slug=slugify(self.name)
+        return super(Tag,self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title=models.CharField(max_length=200)
     content=models.TextField()
