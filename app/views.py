115b2d7faf68f3e27652db7010f12570ef37ede1
Sharique <shariquereza786@gmail.com>
2024-11-18 14:59:02 +0530

    addedd new posts as per post date & popular posts as per view_count

--- app/views.py
+++ app/views.py
@@ -7,7 +7,9 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     posts=Post.objects.all()
-    context={'posts':posts}
+    top_posts=Post.objects.all().order_by('-view_count')[0:3]
+    recent_posts=Post.objects.all().order_by('-last_updates')[0:3]
+    context={'posts':posts,'top_posts':top_posts,'recent_posts':recent_posts}
     return render(request,'app/index.html',context)
 
 def post_page(request,slug):
