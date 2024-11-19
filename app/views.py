3026456e6e989e5873cf80eed30141855141ebb2
Sharique <shariquereza786@gmail.com>
2024-11-19 12:40:38 +0530

    rendered the other tags also in tag.html page

--- app/views.py
+++ app/views.py
@@ -65,5 +65,6 @@ def tag_page(request,slug):
     tag=Tag.objects.get(slug=slug)
     top_posts=Post.objects.filter(tags__in=[tag.id]).order_by('-view_count')[0:2]
     recent_posts=Post.objects.filter(tags__in=[tag.id]).order_by('-last_updates')[0:2]
-    context={'tag':tag,'top_posts':top_posts,'recent_posts':recent_posts}
+    tags=Tag.objects.all()
+    context={'tag':tag,'top_posts':top_posts,'recent_posts':recent_posts,'tags':tags}
     return render(request,'app/tag.html',context)
