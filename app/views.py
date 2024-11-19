180b8a0f1ffb56a78df6e624e0361f6e65481999
Sharique <shariquereza786@gmail.com>
2024-11-19 10:18:10 +0530

    added logic in views for featured blogs

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def index(request):
     posts=Post.objects.all()
     top_posts=Post.objects.all().order_by('-view_count')[0:3]
     recent_posts=Post.objects.all().order_by('-last_updates')[0:3]
+    featured_blogs=Post.objects.filter(is_featured=True)
     subscribe_form=SubscribeForm()
     subscribe_successful=None
 
@@ -19,7 +20,7 @@ def index(request):
             subscribe_successful='Subscribed Successfully'
             subscribe_form=SubscribeForm()
 
-    context={'posts':posts,'top_posts':top_posts,'recent_posts':recent_posts,'subscribe_form':subscribe_form,'subscribe_Successful':subscribe_successful}
+    context={'posts':posts,'top_posts':top_posts,'recent_posts':recent_posts,'subscribe_form':subscribe_form,'subscribe_Successful':subscribe_successful,'featured_blogs':featured_blogs}
     return render(request,'app/index.html',context)
 
 def post_page(request,slug):
