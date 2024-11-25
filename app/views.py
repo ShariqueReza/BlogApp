579135dfc7ffdabcccc5e3b88f2aee7bed3c17c4
Sharique <shariquereza786@gmail.com>
2024-11-25 15:55:59 +0530

    added logic & made some changes for like options

--- app/views.py
+++ app/views.py
@@ -131,3 +131,12 @@ def bookmark_post(request,slug):
     else:
         post.bookmarks.add(request.user)
     return HttpResponseRedirect(reverse('post_page',args=[str(slug)]))
+    
+def like_post(request,slug):
+    post=get_object_or_404(Post,id=request.POST.get('post_id'))
+
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('post_page',args=[str(slug)]))
