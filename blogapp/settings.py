5fe1c61cda79dd3f277d6616a6f76e9dc8be7f50
Sharique <shariquereza786@gmail.com>
2024-11-28 12:20:30 +0530

    added some code in settings.py & collect static files for deploy the project

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -22,12 +22,12 @@ SETTINGS_PATH=os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@q=ks&)jju*w-$0#s$cr$z%ed*mjab1-9i5ee@_i=9w4p$4d0'
+SECRET_KEY = os.getenv("SECRET_KEY",'django-insecure-g@q=ks&)jju*w-$0#s$cr$z%ed*mjab1-9i5ee@_i=9w4p$4d0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_DEVELOPMENT",True)=='True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("APP_HOST"),"127.0.0.1"]
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR / "staticfiles"
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'upload'
 LOGIN_REDIRECT_URL='/'
